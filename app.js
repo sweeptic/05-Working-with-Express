fafd184ab6c8d0720f966880ddc1caf522ccf580
Attila Horvath <sweeptic@gmail.com>
2025-03-19 14:14:41 +0100

    004 Adding Middleware

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+//runs every incoming request
+app.use((req, res, next) => {
+  console.log('middleware 1 runs');
+  next();
+});
+app.use((req, res, next) => {
+  console.log('middleware 2 runs');
+  next();
+});
+app.use((req, res, next) => {
+  console.log('middleware 3 runs');
+  next();
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
