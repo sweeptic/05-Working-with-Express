ff87b2f90e51765ba73c562de961c824ff963678
Attila Horvath <sweeptic@gmail.com>
2025-03-19 14:38:17 +0100

    006 Express.js - Looking Behind the Scenes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 
 const app = express();
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   res.send('hello');
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+// server.listen(3000);
 
-server.listen(3000);
+app.listen(3000);
